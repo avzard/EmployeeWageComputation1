5f8928844d4e2d0e206ee08f1b3c428695a14a5e
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-21 12:50:15 +0530

    Employee Wage Problem

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class Program
+    {
+        // Main Method /Entry point
+        static void Main(string[] args)
+        {
+            //printing message on console
+            Console.WriteLine("Welcome To Employee Wage Computation Program \n");
+
+            //Creating Object for each company and passing value to constructor.
+            CalculateEmpWage company = new CalculateEmpWage();
+            company.AddCompany("Dmart", 30, 120, 25);
+            company.AddCompany("Reliance", 25, 125, 24);
+            company.AddCompany("Amazon", 40, 110, 22);
+            company.GetWage();
+            Console.WriteLine("Total Wage for Company is: " + company.GetTotalWage("Dmart"));
+            Console.ReadLine();
+        }
+    }
+}
