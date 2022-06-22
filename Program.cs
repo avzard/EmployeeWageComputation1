8ef1345bfec4e0870239fa7e35c4dba79fac86b1
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-22 11:51:12 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,14 +6,23 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Computation");
-            int isFullTime = 1;
+            int IS_FULLTIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWages = 0;
             Random rand = new Random();
-            int rn = rand.Next(1, 3);
-            if (isFullTime == 1)
-                Console.WriteLine("Employee Is Presnet");
+            int empChek = rand.Next(1, 3);
+            Console.WriteLine("Welcome To Employee Wage Computation");
+            if (empChek == IS_FULLTIME)
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee Absent");
+            {
+                empHrs = 0;
+            }
+            empWages = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total_Employee_Wages : " + empWages);
         }
     }
 }
